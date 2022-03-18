e3478f1e3aad880fdc878ce8435117d4222cec21
TaThuPhuong <phuongta15099@gmail.com>
2022-03-18 20:32:29 +0700

    //su

--- app/src/main/java/net/fpl/beehome/MainActivity.java
+++ app/src/main/java/net/fpl/beehome/MainActivity.java
@@ -2,6 +2,7 @@ package net.fpl.beehome;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
@@ -88,5 +89,12 @@ public class MainActivity extends AppCompatActivity {
             drawer.openDrawer(GravityCompat.START);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
     }
 }
