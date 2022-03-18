cf9cd0a7ffa2b3cde7dffe07fe8b3210af811a82
TaThuPhuong <phuongta15099@gmail.com>
2022-03-18 20:29:47 +0700

    //su

--- app/src/main/java/net/fpl/beehome/MainActivity.java
+++ app/src/main/java/net/fpl/beehome/MainActivity.java
@@ -1,6 +1,8 @@
 package net.fpl.beehome;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
@@ -8,6 +10,11 @@ import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -16,14 +23,70 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import net.fpl.beehome.databinding.ActivityMainBinding;
+import net.fpl.beehome.ui.Phong.PhongFragment;
+import net.fpl.beehome.ui.home.HomeFragment;
+import net.fpl.beehome.ui.thuChi.thuChiFragment;
 
 public class MainActivity extends AppCompatActivity {
-
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    DrawerLayout drawer;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        drawer = findViewById(R.id.drawer_layout);
+        toolbar = findViewById(R.id.toolbar);
+
+        //        set toolbar thay the cho actionBar
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        ////        dung fragment PhieuMuon lam home
+        FragmentManager manager = getSupportFragmentManager();
+        HomeFragment h_fragment = new HomeFragment();
+        manager.beginTransaction()
+                .replace(R.id.nav_host_fragment_content_main, h_fragment)
+                .commit();
+
+        NavigationView nv = findViewById(R.id.nav_view);
+
+        //        Dieu huong Navigation
+        nv.setNavigationItemSelectedListener((item) ->{
+
+            switch (item.getItemId()){
+                case R.id.nav_home:
+                    HomeFragment homeFragment = new HomeFragment();
+                    manager.beginTransaction().replace(R.id.nav_host_fragment_content_main,homeFragment).commit();
+                    break;
+
+                case R.id.nav_phòng:
+                    PhongFragment phongFragment = new PhongFragment();
+                    manager.beginTransaction().replace(R.id.nav_host_fragment_content_main,phongFragment).commit();
+                    break;
+
+                case R.id.nav_thuChi:
+                    thuChiFragment chiFragment = new thuChiFragment();
+                    manager.beginTransaction().replace(R.id.nav_host_fragment_content_main,chiFragment).commit();
+                    break;
+            }
+            drawer.closeDrawers();
+            return true;
+        });
+
+
+    }
+
+    //nhấn vào icon menu thì mở ra
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            drawer.openDrawer(GravityCompat.START);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
