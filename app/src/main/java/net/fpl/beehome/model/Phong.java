2cdd646be50f1aba7f64141ded8d37760fb15beb
kaiyuan997@gmail.com <kaiyuan997@gmail.com>
2022-03-21 20:42:04 +0700

    giao diện home fragment

--- app/src/main/java/net/fpl/beehome/model/Phong.java
+++ app/src/main/java/net/fpl/beehome/model/Phong.java
@@ -1,11 +1,12 @@
 package net.fpl.beehome.model;
 
 public class Phong {
-    private String IDPhong, soPhong, trangThai;
+    private String IDPhong, soPhong, trangThai, vatTu;
     private int giaPhong, soDienDau, soNuocDau;
     public static final String TB_NAME = "tb_phong";
     public static final String COL_ID = "id_phong";
     public static final String COL_SO_PHONG = "so_phong";
+    public static final String COL_VAT_TU = "vat_tu";
     public static final String COL_TRANG_THAI = "trang_thai";
     public static final String COL_GIA_PHONG = "gia_phong";
     public static final String COL_SO_DIEN_DAU = "so_dien_dau";
@@ -14,10 +15,11 @@ public class Phong {
     public Phong() {
     }
 
-    public Phong(String IDPhong, String soPhong, String trangThai, int giaPhong, int soDienDau, int soNuocDau) {
+    public Phong(String IDPhong, String soPhong, String trangThai, String vatTu, int giaPhong, int soDienDau, int soNuocDau) {
         this.IDPhong = IDPhong;
         this.soPhong = soPhong;
         this.trangThai = trangThai;
+        this.vatTu = vatTu;
         this.giaPhong = giaPhong;
         this.soDienDau = soDienDau;
         this.soNuocDau = soNuocDau;
@@ -45,6 +47,14 @@ public class Phong {
 
     public void setTrangThai(String trangThai) {
         this.trangThai = trangThai;
+    }
+
+    public String getVatTu() {
+        return vatTu;
+    }
+
+    public void setVatTu(String vatTu) {
+        this.vatTu = vatTu;
     }
 
     public int getGiaPhong() {
--- app/src/main/java/net/fpl/beehome/ui/home/HomeFragment.java
+++ app/src/main/java/net/fpl/beehome/ui/home/HomeFragment.java
@@ -1,22 +1,45 @@
 package net.fpl.beehome.ui.home;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import net.fpl.beehome.MainActivity;
 import net.fpl.beehome.R;
 
 public class HomeFragment extends Fragment {
+    ImageView btnDichVu, btnNguoiThue, btnHoaDon, btnSuCo, btnHopDong, btnHuongDan;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
         return v;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btnDichVu = view.findViewById(R.id.btn_dich_vu);
+        btnNguoiThue = view.findViewById(R.id.btn_nguoi_thue);
+        btnHoaDon = view.findViewById(R.id.btn_hoa_don);
+        btnSuCo = view.findViewById(R.id.btn_su_co);
+        btnHopDong = view.findViewById(R.id.brn_hop_dong);
+        btnHuongDan = view.findViewById(R.id.btn_huong_dan);
+        btnDichVu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
