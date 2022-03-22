dd867ab6a9b76e2121c9adb544c0fe273f608d97
TaThuPhuong <phuongta15099@gmail.com>
2022-03-22 10:32:01 +0700

    //su_logo

---
+++ app/src/main/java/net/fpl/beehome/detail/HoaDonMain.java
@@ -0,0 +1,16 @@
+package net.fpl.beehome.detail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import net.fpl.beehome.R;
+
+public class HoaDonMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hoa_don_main);
+    }
+}
