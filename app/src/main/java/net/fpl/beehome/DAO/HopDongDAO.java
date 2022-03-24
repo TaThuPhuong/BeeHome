1d26f790b46839266f111483fe4b1472d944d612
BUITIEN <tienlsd123@gmail.com>
2022-03-24 21:45:18 +0700

    hopdong-suco2

--- app/src/main/java/net/fpl/beehome/DAO/HopDongDAO.java
+++ app/src/main/java/net/fpl/beehome/DAO/HopDongDAO.java
@@ -5,10 +5,13 @@ import android.content.Intent;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.auth.User;
@@ -29,24 +32,35 @@ public class HopDongDAO {
 
     public ArrayList<HopDong> getAll(){
         ArrayList<HopDong> arr = new ArrayList<>();
-
-        db.collection(HopDong.TB_NAME)
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
+        db.collection(HopDong.TB_NAME).addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                arr.clear();
+                for (QueryDocumentSnapshot document : value) {
                                 HopDong objHopDong = document.toObject(HopDong.class);
-//
                                 arr.add(objHopDong);
                                 Log.d("aaaaaaa", document.getId() + " => " + document.getData());
-                            }
-                        } else {
-                            Log.d("aaaaaaa", "Error getting documents: ", task.getException());
-                        }
-                    }
-                });
+                }
+            }
+        });
+
+//        db.collection(HopDong.TB_NAME)
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//                                HopDong objHopDong = document.toObject(HopDong.class);
+//
+//                                arr.add(objHopDong);
+//                                Log.d("aaaaaaa", document.getId() + " => " + document.getData());
+//                            }
+//                        } else {
+//                            Log.d("aaaaaaa", "Error getting documents: ", task.getException());
+//                        }
+//                    }
+//                });
 
         return arr;
     }
