fb6fa9e4fdbf49334daa2f3c8a9ebbe4ba025df9
cuongvv205 <101417205+cuongvv205@users.noreply.github.com>
2022-03-25 22:01:39 +0700

    dich vu

--- app/src/main/java/net/fpl/beehome/ui/thongKe/thongKeFragment.java
+++ app/src/main/java/net/fpl/beehome/ui/thongKe/thongKeFragment.java
@@ -1,24 +1,64 @@
 package net.fpl.beehome.ui.thongKe;
 
 
+import android.app.DatePickerDialog;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
 import net.fpl.beehome.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class thongKeFragment extends Fragment {
+
+    TextView tvTuNgay, tvDenNgay;
+    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_thong_ke, container, false);
-        return v;
 
+        tvTuNgay = v.findViewById(R.id.tv_tuNgay);
+        tvDenNgay = v.findViewById(R.id.tv_denNgay);
+
+        Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int date = c.get(Calendar.DATE);
+
+        tvTuNgay.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog dialog = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+
+                    }
+                }, year, month, date);
+            }
+        });
+
+        tvDenNgay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        return v;
 
 
     }
