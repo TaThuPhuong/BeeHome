1204fad98c942e2b8086e1329ac561f5a451dfe0
BUITIEN <tienlsd123@gmail.com>
2022-04-08 01:55:38 +0700

    layout7

--- app/src/main/java/net/fpl/beehome/ui/doiMatKhau/DoiMatKhauFragment.java
+++ app/src/main/java/net/fpl/beehome/ui/doiMatKhau/DoiMatKhauFragment.java
@@ -1,5 +1,6 @@
 package net.fpl.beehome.ui.doiMatKhau;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -9,11 +10,13 @@ import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -23,14 +26,16 @@ import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import net.fpl.beehome.Login_Activity;
 import net.fpl.beehome.ProgressBarLoading;
 import net.fpl.beehome.R;
 import net.fpl.beehome.ui.phong.PhongFragment;
 
 
 public class DoiMatKhauFragment extends Fragment {
-    LinearLayout lnL;
-    Animation animation;
+
+    ConstraintLayout constraintLayout;
+    Animation animation, animation2;
     TextInputLayout edMKMoi, edMKNhapLai;
     Button btnLuu, btnXoa;
     FirebaseAuth fba;
@@ -38,6 +43,7 @@ public class DoiMatKhauFragment extends Fragment {
     String email;
     PhongFragment phongFragment;
     ProgressBarLoading progressBarLoading;
+    ImageView logo, img1;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -48,13 +54,14 @@ public class DoiMatKhauFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_doi_mat_khau, container, false);
-        lnL = v.findViewById(R.id.aniLogin);
-        animation = AnimationUtils.loadAnimation(getContext(), R.anim.login);
-        lnL.startAnimation(animation);
+
         return v;
     }
 
     public void init(View view) {
+        logo = view.findViewById(R.id.img_logo);
+        img1 = view.findViewById(R.id.img1);
+        constraintLayout = view.findViewById(R.id.anichangepass);
         fba = FirebaseAuth.getInstance();
         user = FirebaseAuth.getInstance().getCurrentUser();
         phongFragment = new PhongFragment();
@@ -67,6 +74,12 @@ public class DoiMatKhauFragment extends Fragment {
         btnXoa = view.findViewById(R.id.btn_clear);
         edMKMoi.setError(null);
         edMKNhapLai.setError(null);
+
+        animation = AnimationUtils.loadAnimation(view.getContext(), R.anim.frombottom);
+        animation2 = AnimationUtils.loadAnimation(view.getContext(), R.anim.fromtop);
+        constraintLayout.startAnimation(animation);
+        img1.startAnimation(animation2);
+        logo.startAnimation(animation2);
     }
 
     @Override
@@ -94,12 +107,22 @@ public class DoiMatKhauFragment extends Fragment {
                     progressBarLoading.hideLoaing();
 
                     if (TextUtils.isEmpty(strMKMoi)) {
-                        edMKMoi.setError("Nhập mật khẩu mới");
+                        edMKMoi.setError("Dữ liệu không được bỏ trống");
+                        return;
+                    }
+
+                    if (strMKMoi.length() < 6){
+                        edMKMoi.setError("Mật khẩu phải trên 6 ký tự");
+                        return;
                     }
                     if (TextUtils.isEmpty(strMKNhapLai)) {
-                        edMKNhapLai.setError("Nhập lại mật khẩu");
+                        edMKNhapLai.setError("Dữ liệu không được bỏ trống");
+                        return;
                     }
-                    return;
+                    if (strMKNhapLai.equals(strMKMoi)){
+                        edMKNhapLai.setError("Mật khẩu phải trên 6 ký tự");
+                        return;
+                    }
                 } else {
                     if (strMKMoi.equals(strMKNhapLai)) {
                         user.updatePassword(strMKMoi).addOnCompleteListener(new OnCompleteListener<Void>() {
@@ -107,7 +130,9 @@ public class DoiMatKhauFragment extends Fragment {
                             public void onComplete(@NonNull Task<Void> task) {
                                 progressBarLoading.hideLoaing();
                                 if (task.isSuccessful()) {
-                                    Toast.makeText(getContext(), "Cập nhập mật khẩu thành công", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(view.getContext(), Login_Activity.class);
+                                    startActivity(intent);
+                                    Toast.makeText(getContext(), "Đổi mật khẩu thành công", Toast.LENGTH_SHORT).show();
                                     clear();
                                 }
                             }
